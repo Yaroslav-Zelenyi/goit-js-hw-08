00afb14a6150a7e84fdfcfb0131652e86b21d457
Yaroslav-Zelenyi <zelenyiyaroslav@gmail.com>
2023-02-23 19:12:14 +0200

    Create feedback/js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+let formData = {};
+
+function handleFormData(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+form.addEventListener('input', throttle(handleFormData, 500));
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+  console.log(formData);
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  formData = {};
+}
+
+const localForm = localStorage.getItem('feedback-form-state');
+if (localForm) {
+  try {
+    formData = JSON.parse(localForm);
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+  emailInput.value = formData.email || '';
+  messageInput.value = formData.message || '';
+}
