ab7affa6491d8a80cdba99484857f1e9986d292d
Yaroslav-Zelenyi <zelenyiyaroslav@gmail.com>
2023-02-23 11:47:58 +0200

    Create video/js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,1 +1,34 @@
+import { galleryItems } from "./gallery-items.js";
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+function CreateGalleryMarckup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+  `;
+    })
+    .join("");
+}
+
+galleryEl.insertAdjacentHTML("afterbegin", CreateGalleryMarckup(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionSelector: "img",
+  captionDelay: 250,
+});
