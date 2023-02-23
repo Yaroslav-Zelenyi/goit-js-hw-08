0bacd5ea30f766e76989e3c61d1f041b902c17ea
Yaroslav-Zelenyi <zelenyiyaroslav@gmail.com>
2023-02-23 19:43:47 +0200

    02/JS

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,16 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
-const iframe = document.querySelector('#vimeo-player');
+const iframe = document.querySelector("#vimeo-player");
 const player = new Player(iframe);
 
 const onPlay = function (data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem("videoplayer-current-time", data.seconds);
   //  console.log(data.seconds);
 };
-player.on('timeupdate', throttle(onPlay, 1000));
+player.on("timeupdate", throttle(onPlay, 1000));
 
-const localStorTime = localStorage.getItem('videoplayer-current-time');
+const localStorTime = localStorage.getItem("videoplayer-current-time");
 if (localStorTime) {
   player.setCurrentTime(localStorTime);
 }
